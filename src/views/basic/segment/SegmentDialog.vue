0a7fde7ce012961f5efb4678b406ee9a5bc629e0
陈阳 <chenyang@singsingenglish.com>
2020-06-15 15:03:43 +0800

    no message

--- src/views/basic/segment/SegmentDialog.vue
+++ src/views/basic/segment/SegmentDialog.vue
@@ -1,14 +1,13 @@
 <template>
-
   <el-dialog
     class="segement-dialog"
     top="5vh"
     :title="title"
     :visible.sync="dialogData.show"
     :close-on-click-modal="false"
-    append-to-body>
+    append-to-body
+  >
     <div>
-
       <el-form :model="form" label-width="80px" size="small">
         <el-form-item label="名称">
           <el-input v-model="form.title" placeholder="请输入"></el-input>
@@ -19,7 +18,8 @@
             v-model="form.status"
             :active-value="1"
             :inactive-value="0"
-            active-color="#13ce66">
+            active-color="#13ce66"
+          >
           </el-switch>
         </el-form-item>
 
@@ -29,7 +29,8 @@
               v-for="item in SEGMENT_TYPE_ENUM"
               :key="item.key"
               :label="item.value"
-              :value="item.key">
+              :value="item.key"
+            >
             </el-option>
           </el-select>
         </el-form-item>
@@ -51,148 +52,139 @@
             :show-file-list="false"
             :http-request="uploadFile"
             list-type="picture-card"
-            multiple>
+            multiple
+          >
             <el-image
-              style="width: 100%; height: 100%"
+              style="width: 100%; height: 100%;"
               fit="contain"
               v-if="form.preview_image"
-              :src="form.preview_image">
+              :src="form.preview_image"
+            >
             </el-image>
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
         </el-form-item>
-
       </el-form>
-
     </div>
     <div slot="footer" v-if="dialogData.type !== 'view'">
       <el-button @click="dialogToggle">取 消</el-button>
       <el-button type="primary" @click="dialogSave">确 定</el-button>
     </div>
   </el-dialog>
-
 </template>
 
 <script>
-  import commonMessage from "@/views/common/commonMessage"
-  import menuRole from "@/views/common/menuRole"
-  import {upload} from "@api/upload"
-  import {getEnum} from "@util/storage"
+import commonMessage from "@/views/common/commonMessage";
+import menuRole from "@/views/common/menuRole";
+import { upload } from "@api/upload";
+import { getEnum } from "@util/storage";
 
-  const FORM_DEFAULT = {
-    title: '',
-    type: '',
-    status: 1,
-    preview_image: '',
-  }
+const FORM_DEFAULT = {
+  title: "",
+  type: "",
+  status: 1,
+  preview_image: "",
+};
 
-  export default {
-    name: "SegmentDialog",
+export default {
+  name: "SegmentDialog",
 
-    mixins: [commonMessage, menuRole],
+  mixins: [commonMessage, menuRole],
 
-    props: {
-      dialogData: {
-        type: Object,
-        default: {
-          show: false,
-          type: '',
-          param: {
-            id: 0
-          }
-        }
+  props: {
+    dialogData: {
+      type: Object,
+      default: {
+        show: false,
+        type: "",
+        param: {
+          id: 0,
+        },
+      },
+    },
+  },
+
+  data() {
+    return {
+      title: "",
+
+      SEGMENT_TYPE_ENUM: getEnum("SegmentTypeEnum"),
+
+      form: JSON.parse(JSON.stringify(FORM_DEFAULT)),
+    };
+  },
+
+  watch: {
+    "dialogData.show"(val) {
+      if (val) this.init();
+    },
+  },
+
+  methods: {
+    init() {
+      if (this.dialogData.type == "add") {
+        this.title = "新增环节模板";
+        this.form = JSON.parse(JSON.stringify(FORM_DEFAULT));
+      } else if (this.dialogData.type == "edit") {
+        this.title = "编辑环节模板";
+        this.form = this.dialogData.param;
+      } else if (this.dialogData.type == "view") {
+        this.title = "查看环节模板";
+        this.form = this.dialogData.param;
       }
     },
 
-    data() {
-      return {
-        title: '',
-
-        SEGMENT_TYPE_ENUM: getEnum('SegmentTypeEnum'),
-
-        form: JSON.parse(JSON.stringify(FORM_DEFAULT))
-      }
+    uploadFile(e) {
+      upload(e.file).then((res) => {
+        this.form.preview_image = res.url;
+      });
     },
 
-    watch: {
-      'dialogData.show'(val) {
-        if (val) this.init();
-      }
+    openMedia(url) {
+      console.log("media url", url);
+      window.open(url, "_blank");
     },
 
-    methods: {
-      init() {
-        if (this.dialogData.type == 'add') {
-          this.title = '新增环节模板';
-          this.form = JSON.parse(JSON.stringify(FORM_DEFAULT))
-        } else if (this.dialogData.type == 'edit') {
-          this.title = '编辑环节模板';
-          this.form = this.dialogData.param
-        } else if (this.dialogData.type == 'view') {
-          this.title = '查看环节模板';
-          this.form = this.dialogData.param
-        }
-      },
+    dialogToggle() {
+      this.dialogData.show = !this.dialogData.show;
+    },
 
-
-      uploadFile(e) {
-        upload(e.file).then(res => {
-          this.form.preview_image = res.url
-        })
-      },
-
-      openMedia(url) {
-        console.log('media url', url);
-        window.open(url, '_blank')
-      },
-
-      dialogToggle() {
-        this.dialogData.show = !this.dialogData.show
-      },
-
-      dialogSave() {
-
-        // 校验
-        /*if () {
+    dialogSave() {
+      // 校验
+      /*if () {
 
         }*/
 
+      let api,
+        form = this.form,
+        json = {
+          title: form.title,
+          type: form.type,
+          status: form.status,
+          preview_image: form.preview_image,
+        };
 
-        let
-          api,
-          form = this.form,
-          json = {
-            title: form.title,
-            type: form.type,
-            status: form.status,
-            preview_image: form.preview_image,
-          };
+      if (this.dialogData.param.id) {
+        json.id = this.dialogData.param.id;
+        api = this.ApiBasic.putSegment;
+      } else {
+        api = this.ApiBasic.postSegment;
+      }
 
-        if (this.dialogData.param.id) {
-          json.id = this.dialogData.param.id;
-          api = this.ApiBasic.putSegment;
-        } else {
-          api = this.ApiBasic.postSegment;
-        }
-
-        api(json).then(res => {
-          this.$message({
-            type: 'success',
-            message: '保存成功'
-          });
-          this.dialogToggle();
-          this.$parent.getData();
-        })
-
-      },
-
-    }
-
-  }
+      api(json).then((res) => {
+        this.$message({
+          type: "success",
+          message: "保存成功",
+        });
+        this.dialogToggle();
+        this.$parent.getData();
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  .segment-dialog {
-
-  }
+.segment-dialog {
+}
 </style>
