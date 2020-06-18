9e84b6e3c703d773c619856cb2a9cf33d3a1704b
rulin <zhaorulin@singsingenglish.com>
2020-06-18 20:49:54 +0800

    update

--- src/views/main.vue
+++ src/views/main.vue
@@ -21,6 +21,18 @@
   import AppMain from "@/views/AppMain";
   import Menu from "@/components/menu/Menu";
   import {mapActions, mapGetters} from "vuex";
+  import base from "@api/base"
+  import user from "@api/user"
+  import other from "@api/other"
+  import production from "@api/production"
+  import menu from "@api/menu"
+  import order from "@api/order"
+  import report from "@api/report"
+  import basic from "@api/basic"
+  import resource from "@api/resource"
+  import course from "@api/course"
+  import teach from "@api/teach"
+  import {mapMutations} from "vuex";
 
   const sideBarWidth = "240px";
   export default {
@@ -34,9 +46,54 @@
       AppMain,
       Menu,
     },
-    computed: {
+    computed: {},
+
+    provide() {
+      return {
+        dictoryObj: this.dictoryObj,
+      };
     },
+
+    mounted() {
+      this.getEnum();
+    },
+
     methods: {
+
+      ...mapMutations("course", {
+        set_dictoryObj: "set_dictoryObj",
+      }),
+
+      /*
+       * BooleanEnum  是否
+       * DubbingTypeEnum  配音类型
+       * ElementTypeEnum  课程元素类型
+       * EvaluationSwitchTypeEnum  评测切换方式
+       * QuestionMaterialTypeEnum  评测素材类型
+       * SegmentLeadTypeEnum  环节引导类型
+       * SegmentTypeEnum  环节类型
+       * */
+      async getEnum() {
+        let param = {
+          enum: [
+            "BooleanEnum",
+            "DubbingTypeEnum",
+            "ElementTypeEnum",
+            "EvaluationSwitchTypeEnum",
+            "QuestionMaterialTypeEnum",
+            "SegmentLeadTypeEnum",
+            "SegmentTypeEnum",
+            "CourseTypeEnum",
+          ],
+        };
+        let o = {};
+        let data = await base.getEnum(param);
+        data.forEach((item) => {
+          o[item.name] = item.items;
+        });
+        this.set_dictoryObj(o);
+      },
+
       handleOpen() {
       },
       handleClose() {
