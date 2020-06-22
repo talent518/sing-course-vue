6595868c44f6288e752725862ca9eb7bcf931311
张冰 <zhangbing@singsingenglish.com>
2020-06-22 13:57:58 +0800

    fixed

--- src/config/test.js
+++ src/config/test.js
@@ -1,7 +1,7 @@
 const config = {
     apiUrl: 'http://course-api.test.changchangenglish.com/',
     mediaUrl: 'https://media.changchangenglish.com/',
-    ssoUrl: 'https://sso.test.changchangenglish.com/',
+    ssoUrl: 'http://sso.test.changchangenglish.com/',
 };
 
 export default config;
